1bf07aec0da2c3e9bf7d6c6514130d0c8f13f034
Matt <meboss212@live.com>
2018-03-17 14:31:44 -0400

    reduced array size for testing

--- app/src/main/java/com/ourideas/android/animalroundup/MainActivity.java
+++ app/src/main/java/com/ourideas/android/animalroundup/MainActivity.java
@@ -1,4 +1,6 @@
-package com.example.android.animalapp;
+package com.ourideas.android.animalroundup;
+
+import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.media.MediaPlayer;
@@ -21,40 +23,11 @@ public class MainActivity extends AppCompatActivity {
     Random r;
 
     Integer[] images = {
-            R.drawable.armadillo,
-            R.drawable.bat,
-            R.drawable.cat,
-            R.drawable.chimp,
-            R.drawable.cow,
+
             R.drawable.donkey,
-            R.drawable.duck,
-            R.drawable.elephant,
-            R.drawable.flamingo,
-            R.drawable.fox,
-            R.drawable.frog,
-            R.drawable.goose,
-            R.drawable.gorilla,
-            R.drawable.hippo,
-            R.drawable.horse,
             R.drawable.hyena,
-            R.drawable.impala,
-            R.drawable.lemur,
             R.drawable.lion,
-            R.drawable.lizard,
-            R.drawable.orca,
-            R.drawable.ostrich,
-            R.drawable.panda,
-            R.drawable.parrot,
-            R.drawable.peacock,
-            R.drawable.penguin,
-            R.drawable.pig,
-            R.drawable.platypus,
-            R.drawable.puffin,
-            R.drawable.seal,
-            R.drawable.sloth,
-            R.drawable.walrus,
-            R.drawable.warthog,
-            R.drawable.zebra
+
     };
 
     int currentImage = 0, lastImage = 0;
@@ -64,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        animalSound = MediaPlayer.create(this, R.raw.bear2);
+        animalSound = MediaPlayer.create(this, R.raw.hyena);
         animalSound.start();
 
         imageView = (ImageView) findViewById(R.id.imageView);
